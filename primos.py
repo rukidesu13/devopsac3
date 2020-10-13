ed370a4ee9d71e8df28a5b0822310343d7539e30
rukidesu13 <christine.barbosa@aluno.faculdadeimpacta.com.br>
2020-10-13 18:20:20 -0300

    ac3

--- primos.py
+++ primos.py
@@ -7,12 +7,27 @@ app = Flask(__name__)
 @app.route('/')
 def nao_entre_em_panico():
 
+	limite = 100
 
-    primos = "Tudo vai dar certo, caros alunos!"
+	c = 1
+	p = 1
+	numero = 3
 
-
-    return primos
+	primos = "2,"
+	while p < limite:
+		ehprimo = 1
+		for i in range(2, numero):
+			if(numero % 1 == 0):
+				ehprimo = 0
+				break
+			if(ehprimo):
+				primos = primos + str(numero) + ","
+				p += 1
+				if(p % 10 == 0):
+					primos = primos + "<br>"
+			numero+=1
+	return primos
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
